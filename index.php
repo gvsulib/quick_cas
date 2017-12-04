a1e37a8338488a79d80600cb89f26a77b708bb71
kyle felker <felkerk@gvsu.edu>
2017-12-04 08:55:19 -0500

    bug fix

--- index.php
+++ index.php
@@ -7,9 +7,8 @@
 // Connect to database
 	
 	include_once('CASold/CAS.php');
-	$filename = "caslog.log";
 	
-	phpCAS::setVerbose(true);
+	
 
 
 	if (isset($_REQUEST['logout'])) {
@@ -20,6 +19,7 @@
 			
 			// Send user to CAS
 			phpCAS::client(CAS_VERSION_2_0,'eis-test.gvsu.edu',443,'/auth/', false);
+			phpCAS::setVerbose(true);
 			$filename = 'caslog.txt';
 			phpCAS::setDebug($filename);
 
