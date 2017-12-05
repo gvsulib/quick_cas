5f54b2bddfb4344a910bef4d93978e1ab2881895
kyle felker <felkerk@gvsu.edu>
2017-12-05 10:32:50 -0500

    trying different code

--- index.php
+++ index.php
@@ -12,32 +12,25 @@
 
 
 	if (isset($_REQUEST['logout'])) {
-		setcookie("login", "", -3600, "/");
+		
 		phpCAS::logout();
-	} else {
-		//echo "making it to CAS section";
+	} 
+
+
+// Send user to CAS
+phpCAS::client(CAS_VERSION_2_0,'eis-test.gvsu.edu',443,'/auth/', false);
 			
-			// Send user to CAS
-			phpCAS::client(CAS_VERSION_2_0,'eis-test.gvsu.edu',443,'/auth/', false);
-			
-			$filename = 'caslog.txt';
-			phpCAS::setDebug($filename);
+$filename = 'caslog.txt';
+phpCAS::setDebug($filename);
 
-			// initialize phpCAS
-			
-
-			// no SSL validation for the CAS server
-			phpCAS::setNoCasServerValidation();
-
-			// force CAS authentication
-			phpCAS::forceAuthentication();
-
+// initialize phpCAS
 			
 
-	}
-	
+// no SSL validation for the CAS server
+phpCAS::setNoCasServerValidation();
 
-
+// force CAS authentication
+phpCAS::forceAuthentication();
 
 // at this step, the user has been authenticated by the CAS server
 // and the user's login name can be read with phpCAS::getUser().
